3e043e28108d845b0259c1527a58cd86464ce9de
fingermustache <jamesgreene@defenseunicorns.com>
2025-06-23 10:22:23 -0700

    feat: update license

--- 3-solutions/svelte-app-demo/src/routes/sverdle/how-to-play/+page.ts
+++ 3-solutions/svelte-app-demo/src/routes/sverdle/how-to-play/+page.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2025 Defense Unicorns
+ * SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+ */
+
 import { dev } from '$app/environment';
 
 // we don't need any JS on this page, though we'll load
--- 3-solutions/svelte-app-demo/svelte.config.js
+++ 3-solutions/svelte-app-demo/svelte.config.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2025 Defense Unicorns
+ * SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+ */
+
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
